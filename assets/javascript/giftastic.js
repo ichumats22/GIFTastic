b058e075ab9dce7144264d67f0a6e2a17c5a996c
ichumats <ivanna.chumatschko22@gmail.com>
2019-11-06 12:39:17 -0700

    initial file creation and pseudocode

---
+++ assets/javascript/giftastic.js
@@ -0,0 +1,20 @@
+//VARIABLES
+//================================================================================================================================================
+  //var topics = []
+
+//FUNCTIONS
+//================================================================================================================================================
+  //Create a function that dynamically populates the button div from the topics array when the page loads
+
+  //Create a function that pulls 10 gifs related to the user input (from click event) from the Giphy API and appends them to the gif div along with their rating
+    //AJAX
+
+  //Create a function that adds the user's input from the "Add a topic" form into the "topics" array
+
+//LOGIC
+//================================================================================================================================================
+//When the page load, populate the buttons 
+
+//Listen for clicks on the topics buttons
+
+//listen for clicks on the "add a topic" button
